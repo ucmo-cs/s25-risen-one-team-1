8512bc0e34fb8a708e4d484745b0613b09dd66c8
Luke <Strazzluke34@gmail.com>
2025-03-06 17:25:56 -0600

    Complete Crud functionality for Employees Table

--- base-senior-project-main/backend/src/handlers/EmployeeCRUD/createEmployee.js
+++ base-senior-project-main/backend/src/handlers/EmployeeCRUD/createEmployee.js
@@ -1,10 +1,10 @@
 'use strict'
 
-/*Used for creating and updating projects
-Send a body with a ProjectID: Int, ProjectName: String, EmployeesID: List of ints
-URL/createProject
-***Both creating and updating require you to send all the data needed for a full project***
-*201: project successfully created*
+/*Used for creating and updating employees
+Send a body with a EmployeeID: Int, EmployeeName: String, ProjectID: Int
+URL/createEmployee
+***Both creating and updating require you to send all the data needed for a full employee***
+*201: employee successfully created*
 *400: bad request - client error - missing attributes in body*
 *500: Serverside error*
 */
@@ -14,19 +14,19 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
     const body = JSON.parse(event.body);
+    const EmployeeID = body.EmployeeID;
+    const EmployeeName = body.EmployeeName;
     const ProjectID = body.ProjectID;
-    const ProjectName = body.ProjectName;
-    const EmployeesID = body.EmployeesID;
     const params = {//params: how we find the table and item to create/update
-        TableName: process.env.PROJECTS_TABLE,
+        TableName: process.env.EMPLOYEES_TABLE,
         Item: {//the item/row to be created
-        ProjectID: ProjectID,
-        ProjectName: ProjectName,
-        EmployeesID: EmployeesID
+        EmployeeID: EmployeeID,
+        EmployeeName: body.EmployeeName,
+        ProjectID: body.ProjectID
         }
     };
 
-    if(!ProjectID || !ProjectName || !EmployeesID){//if one of these is empty, return bad request
+    if(!ProjectID || !EmployeeName || !EmployeeID){//if one of these is empty, return bad request
         return {
             statusCode: 400,//400: bad request
             body: JSON.stringify("Bad Request, missing attributes in body.")
@@ -40,14 +40,14 @@ module.exports.handler = async (event) => {
             statusCode: 201,//201: created
             body: JSON.stringify({
                 message: 'Created Successfully',
-                ProjectID: ProjectID,//returning the data for validity
-                ProjectName: ProjectName,
-                Employees: EmployeesID
+                EmployeeID: EmployeeID,//returning the data for validity
+                EmployeeName: EmployeeName,
+                ProjectID: ProjectID
             })
         }
     }catch (e) {
         return {
-            statusCode: 500,//internal server error
+            statusCode: 500,//500: bad request
             body: JSON.stringify("Internal server error: " + e)
         }
     }
