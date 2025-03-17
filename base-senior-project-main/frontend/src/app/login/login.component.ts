22547db4e58f44d080afce63c86b28eccd671917
connorsloan <connorsloan42@gmail.com>
2025-03-16 20:41:53 -0500

    Edit a comment for clarity

--- base-senior-project-main/frontend/src/app/login/login.component.ts
+++ base-senior-project-main/frontend/src/app/login/login.component.ts
@@ -74,9 +74,8 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) { }
 
   login(event: Event) {
-    event.preventDefault(); // Prevent the form from submitting -- for debug->
-    //-> enabling this allows to see console errors from login form,->
-    //->but also disables the form validation popups
+    event.preventDefault(); // Prevents default JavaScript form submission ->
+    //-> also breaks the warning for empty username/password fields
     this.authService.login(this.username, this.password)
       .subscribe({
         next: (success) => {
